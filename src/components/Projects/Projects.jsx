ec58a4417d0c384af64a4f064616d89c4f00d0fb
Thanos Theodosiou <thanosthd@gmail.com>
2021-06-12 17:30:44 +0300

    Create Projects Section Component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react';
+import cn from 'classnames';
+
+import isVisibleToUser from '../../common/isVisibleToUser';
+
+import styles from './Projects.module.scss';
+
+export default function AboutMe() {
+  const ref = useRef();
+  const isVisible = isVisibleToUser(ref);
+
+  return (
+    <section className={cn({ show: isVisible })} id="projects" ref={ref}>
+      <h2>Things I have built</h2>
+      <div className={styles.content}>
+        Projects
+      </div>
+    </section>
+  );
+}
